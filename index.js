f523a974e478f3ebe8b8c2b4f01d58e1ff41de77
Alan Kerby <alankerby@gmail.com>
2022-04-28 21:48:16 +0100

    fadein added

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
+        document.getElementById("navbar").style.opacity = "1";
     } else {
-        document.getElementById("navbar").style.top = "-10vh";
+        // document.getElementById("navbar").style.top = "-10vh";
+        document.getElementById("navbar").style.opacity = "0";
     }
     prevScrollpos = currentScrollPos;
 }
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.fadein').each(function (i) {
+            var top_of_element = $(this).offset().top;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > top_of_element) {
+                $(this).animate({ 'opacity': '1' }, 50);
+            }
+        });
+    });
+});
