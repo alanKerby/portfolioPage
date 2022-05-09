799c3ac421886f600488420043baf4d6e55e2995
Alan Kerby <alankerby@gmail.com>
2022-05-09 18:51:07 +0100

    portfolio porject added

---
+++ scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.fadein').each(function (i) {
+            var top_of_element = $(this).offset().top;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > top_of_element) {
+                $(this).animate({ 'opacity': '1' }, 500);
+            }
+        });
+    });
+
+    var lastScrollTop = 0;
+    $(window).scroll(function (event) {
+        var st = $(this).scrollTop();
+        if (st > lastScrollTop) {
+            $("nav").fadeOut();
+        } else {
+            $("nav").fadeIn();
+        }
+        lastScrollTop = st;
+    });
+});
+
